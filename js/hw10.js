76a010341f3e7b6ad110e2d0a7fd6e70cd7851e8
Stas <tselinskyis@gmail.com>
2023-03-30 17:24:21 +0200

    Add(class Select)

--- js/hw10.js
+++ js/hw10.js
@@ -1,40 +1,43 @@
 "use strict";
 
 class List {
-  constructor(qttyOfPages) {
-    this.qttyOfPages = qttyOfPages;
+  constructor() {
     this.form = document.createElement("form");
-    this.select = document.createElement("select");
+    this.select = new Select([
+      {
+        title: "Page 1",
+        value: 1,
+      },
+      {
+        title: "Page 2",
+        value: 2,
+      },
+      {
+        title: "Page 3",
+        value: 3,
+      },
+      {
+        title: "Page 4",
+        value: 4,
+      },
+      {
+        title: "Page 5",
+        value: 5,
+      },
+    ]);
     this.input = document.createElement("input");
-    this.div = document.createElement("div");
-    this.div2 = document.createElement("div");
+    this.div2 = document.createElement('div');
   }
-}
-
-class Select extends List {
-  constructor(qttyOfPages) {
-    super(qttyOfPages);
-  }
-
-  create() {
-    document.body.appendChild(this.div);
-    document.body.appendChild(this.div2);
-    this.form.appendChild(this.select);
-    this.form.appendChild(this.input);
-    this.div.appendChild(this.form);
-    this.input.setAttribute("type", "submit");
-    this.input.setAttribute("value", "GET");
-
-    for (let i = 1; i <= this.qttyOfPages; i++) {
-      const option = document.createElement("option");
-      option.textContent = `Страница ${i}`;
-      option.dataset.num = `${i}`;
-
-      this.select.appendChild(option);
-    }
+  createForm() {
+    this.form.appendChild(this.select.element);
+    this.input.type = "submit";
     this.form.appendChild(this.input);
   }
 
+  render() {
+    document.body.appendChild(this.form);
+    this.select.render();
+  }
   dataFromAPI() {
     this.form.addEventListener("submit", async (event) => {
       event.preventDefault();
@@ -43,45 +46,72 @@ class Select extends List {
         uls.remove();
       }
 
-      const page = this.select.options[this.select.selectedIndex].dataset.num; //нашел это решение,хотелось бы понять как оно работает, не додумался)
+      const page = this.select.selectedValue;
       this.input.disabled = true;
-      this.select.disabled = true;
+      this.select.element.disabled = true;
 
       const url = new URL("https://rickandmortyapi.com/api/character");
       url.searchParams.append("page", page);
 
-        try {
-          await wait(3000);
-          const ul = document.createElement("ul");
-          this.div2.innerHTML = "";
-          document.body.appendChild(ul);
+      try {
+        await wait(3000);
+        const ul = document.createElement("ul");
+        this.div2.innerHTML = "";
+        document.body.appendChild(ul);
 
-          let response = await fetch(url);
-          if (!response.ok) {
-            return new Error();
-          }
-          let data = await response.json();
-
-          data.results.forEach((result) => {
-            const li = document.createElement("li");
-            li.textContent = result.name;
-            ul.appendChild(li);
-          });
-          this.div2.appendChild(ul);
-        } catch (e) {
-          console.error('ERROR');
-        } finally {
-          this.input.disabled = false;
-          this.select.disabled = false;
+        let response = await fetch(url);
+        if (!response.ok) {
+          return new Error();
         }
+        let data = await response.json();
+
+        data.results.forEach((result) => {
+          const li = document.createElement("li");
+          li.textContent = result.name;
+          ul.appendChild(li);
+        });
+        this.div2.appendChild(ul);
+      } catch (e) {
+        console.error("ERROR");
+      } finally {
+        this.input.disabled = false;
+        this.select.element.disabled = false;
+        document.body.appendChild(this.div2);
+      }
     });
   }
 }
+
+class Select {
+  constructor(elements) {
+    this.elements = elements;
+    this.element = document.createElement("select");
+    this.selectedValue = elements[0].value;
+    console.log(typeof this.selectedValue );
+  }
+  render() {
+    for (const element of this.elements) {
+      const option = document.createElement("option");
+      option.textContent = element.title;
+      option.value = element.value;
+      this.element.appendChild(option);
+    }
+    this.element.addEventListener("change", (event) => {
+      this.selectedValue = +(event.target.value);
+      // console.log(typeof this.selectedValue );
+    });
+  }
+}
+
+const list = new List();
+list.createForm();
+list.render();
+list.dataFromAPI();
+
+
 ///////////////////////////////////////////////////////////functions
 function wait(ms) {
   return new Promise((r) => setTimeout(() => r(), ms));
 }
+
 ///////////////////////////////////////////////////////////functions call
-const newSelect = new Select(5);
-newSelect.create();
-newSelect.dataFromAPI();
